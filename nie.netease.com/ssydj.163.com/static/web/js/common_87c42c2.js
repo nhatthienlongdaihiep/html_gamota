059b5586403a2f3bfeacadc1e5903510168ea133
Nguyễn Thanh Tân <tannt@appota.com>
2017-04-21 10:08:10 +0700

    add ssygj

--- nie.netease.com/ssydj.163.com/static/web/js/common_87c42c2.js
+++ nie.netease.com/ssydj.163.com/static/web/js/common_87c42c2.js
@@ -1,1 +1,9 @@
-nie.define("common",function(){var e=function(){var e=nie.require("nie.util.shareV5"),t=$("#share_pic").attr("src"),i=$("#share_title").html(),n=$("#share_desc").html();e({fat:"#Mshare",type:2,title:i,img:t,content:n})},t=function(){e()};return{init:t}});
+nie.define("common",function(){
+	var e=function(){
+		var e=nie.require("nie.util.shareV5"),
+		t=$("#share_pic").attr("src"),
+		i=$("#share_title").html(),
+		n=$("#share_desc").html();
+		e({fat:"#Mshare",type:2,title:i,img:t,content:n})
+	},t=function(){e()};return{init:t}
+});
